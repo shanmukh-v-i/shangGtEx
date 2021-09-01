5c888078414bc7329f08b90f987b9c1635822044
shanmukh-v-i <shanmukh.v.i@gmail.com>
2021-09-01 16:50:34 +0530

    java first example Modified

--- JavaFirst.java
+++ JavaFirst.java
@@ -1,8 +1,5 @@
 public class JavaFirst{
 public static void main(String h[]){
-System.out.println("Hello!!! Welcome To Git");
-
-
-}
-
+System.out.println("Hello!!!! Welcome To Git");
+System.out.println("Is it Ok?");
 }
